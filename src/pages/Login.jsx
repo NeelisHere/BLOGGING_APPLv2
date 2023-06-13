3d70899d364195361aad1d3cf7092f71640f79a4
Neelabh Paul <npaul_be19@thapar.edu>
2023-06-13 22:04:13 +0530

    login and register forms are now capable of reciveing data from the form and send to the backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return (
-    <form className="login" action="">
-        <h1>Login</h1>
-        <input type="text" placeholder='username' />
-        <input type="password" placeholder='password' />
-        <button>Login</button>
-    </form>
-  )
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const doLogin = async(e)=>{
+        e.preventDefault()
+        const data = JSON.stringify({username, password})
+        const url = 'http://localhost:5000/login'
+        console.log(data)
+        const status = await fetch(url,{
+            method:'POST',
+            body: data,
+            headers: {'Content-Type': 'application/json'}
+        })
+        console.log(status)
+        setPassword('')
+        setUsername('')
+    }
+    return (
+        <form className="login" onSubmit={doLogin}>
+            <h1>Login</h1>
+
+            <input 
+                type="text" 
+                placeholder='username' 
+                value={username} 
+                onChange={(e)=>{
+                    setUsername(e.target.value);
+                }}
+            />
+
+            <input 
+                type="password" 
+                placeholder='password' 
+                value={password} 
+                onChange={(e)=>{
+                    setPassword(e.target.value);
+                }}
+            />
+            <button>Login</button>
+        </form>
+    )
 }
 
 export default Login
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Register = () => {
+const Register = () => { 
+  const [data, setData] = useState({
+    username:'', 
+    email:'', 
+    password:'', 
+  })
+  const register = async (e) => {
+    e.preventDefault()
+    const url = 'http://localhost:5000/register'
+    console.log(data)
+    await fetch(url, {
+      method:'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
+    })
+    setData({...data, username:'', email:'', password:''})
+  }
   return (
-    <form className="register" action="">
+    <form className="register" onSubmit={register}>
         <h1>Register</h1>
-        <input type="text" placeholder='username' />
-        <input type="password" placeholder='password' />
+        <input 
+          type="text" 
+          placeholder='username' 
+          value={data.username}
+          onChange={(e)=>{
+            setData({...data, username: e.target.value})
+          }} 
+        />
+        <input 
+          type="email" 
+          placeholder='email' 
+          value={data.email}
+          onChange={(e)=>{
+            setData({...data, email: e.target.value})
+          }} 
+        />
+        <input 
+          type="password" 
+          placeholder='password' 
+          value={data.password}
+          onChange={(e)=>{
+            setData({...data, password: e.target.value})
+          }} 
+        />
         <button>Register</button>
     </form>
   )
